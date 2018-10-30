252643fb6db0ea5495d58b9fc011ac0adb0f1a0c
aever32 <aeveraeveraever@gmail.com>
2018-10-30 20:51:37 +0200

    Change in server core

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import trio
 
 HOST = '0.0.0.0'
@@ -5,11 +6,18 @@ PORT = 12345
 BUF_SIZE = 2048
 
 
-async def id_for_client(data):
-    pass
+async def parse_client_data(server_stream, data):
+    new_dict = data.decode('utf-8')
+    new = json.loads(new_dict)
+    if new['id'] == 3:
+        await server_stream.send_all(b'Server do the registration')
+    elif new['name'] == 'abc':
+        await server_stream.send_all(b'Login to the server')
+    else:
+        await server_stream.send_all(b'!EXIT!')
 
 
-async def echo_server(server_stream):
+async def core_server(server_stream):
     print("echo_server : new connection started")
     try:
         while True:
@@ -18,14 +26,15 @@ async def echo_server(server_stream):
             if not data:
                 print("echo_server : connection closed")
                 return
-            await id_for_client(data)
-            print("echo_server : sending data {}" . format(data))
-            await server_stream.send_all(data)
+            await parse_client_data(server_stream, data)
     except Exception as exc:
         print("echo_server : crashed: {} ".format(exc))
 
 
 async def main():
-    await trio.serve_tcp(echo_server, PORT, host=HOST)
+    try:
+        await trio.serve_tcp(core_server, PORT, host=HOST)
+    except KeyboardInterrupt:
+        print('Server was stopped! CTRL + C')
 
 trio.run(main)
