e24e45c3a24e99951a5d7fe3bacc3d5673b032ec
aever32 <aeveraeveraever@gmail.com>
2018-11-30 16:37:00 +0200

    Переделал внутреннюю проверку корректности данных, добавил коментарии

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ connection = trio_mysql.connect(**DB_CONFIG)
 
 
 # Проверка блока регистрации
+# FIXME Нужно доделать проверку блока регистрации
 async def clean_registration(data: dict) -> dict:
     # проверка email по шаблону
     # шаблон Email
@@ -38,39 +39,29 @@ async def clean_registration(data: dict) -> dict:
     address = data['email']
     # результат проверки
     is_valid = pattern.match(address)
-
     # email_len = len(data['email'])
     password_len = len(data['password'])
     nickname_len = len(data['nickname'])
+    # Проверка длины данных пользователя
     if is_valid and (6 <= password_len <= 30) and (3 <= nickname_len <= 20):
-        return {'flag': 'true', 'result': 'email ok'}
+        return {'flag': 'true'}
     else:
         return {'flag': 'false', 'result': 'email failed'}
 
 
 # Проверка блока аутентификации
-async def clean_login(data: dict) -> bool:
-    return True
+async def clean_login(data: dict) -> dict:
+    # FIXME Нужно добавить проверку данных при аутентификации пользователя
+    return {'flag': 'true'}
 
 
 # Проверка блока действий
-async def clean_action(data: dict) -> bool:
+async def clean_action(data: dict) -> dict:
+    # Сверка данных токена от пользователя с глобальным словарем токенов сервера
     if data['token'] == TOKENS.get(data['id']):
-        return True
+        return {'flag': 'true'}
     else:
-        return False
-
-
-# Проверка корректности данных клиента
-async def filter_client_data(data: dict) -> dict:
-    if data['client'] == 'act':
-        return await clean_action(data)
-    elif data['client'] == 'reg':
-        return await clean_registration(data)
-    elif data['client'] == 'log':
-        return await clean_login(data)
-    else:
-        return False
+        return {'flag': 'false'}
 
 
 # Генерация хэш пароля
@@ -79,6 +70,7 @@ async def get_hash_password(password: str) -> str:
 
 
 # Сравнение хэш пароля
+# Пока нигде не используется, нужна ли она вообще?!
 async def compare_password(hashed_password: str, user_password: str) -> bool:
     return hashed_password == hashlib.sha256(user_password.encode('utf-8')).hexdigest()
 
@@ -99,67 +91,94 @@ async def send_json_to_client(server_stream, data: dict):
 
 # Основное действие клиента
 async def action(server_stream, data: dict):
-    if not data.get('email'):
-        result = {'result': 'it`s alive!'}
-        await send_json_to_client(server_stream, result)
+    # Проверка данных на чистоту, если всё в порядке продолжаем пользовательское действие
+    result = await clean_action(data)
+    if result['flag'] == 'true':
+        # временная вставка if, просто для теста
+        if not data.get('email'):
+            result = {'result': 'it`s alive!'}
+            await send_json_to_client(server_stream, result)
+        else:
+            async with connection as conn:
+                async with conn.cursor() as cursor:
+                    sql = "UPDATE users SET updated=now() WHERE email = %s"
+                    # Делаем запрос в БД
+                    await cursor.execute(sql, (data['email']))
+                    # Соединение не автокомитится по умолчанию.
+                    # Поэтому нужно сделать комит для сохранения данных в БД
+                    await conn.commit()
+                    result = {'result': 'server do the action'}
+                    await send_json_to_client(server_stream, result)
     else:
-        async with connection as conn:
-            async with conn.cursor() as cursor:
-                sql = "UPDATE users SET updated=now() WHERE email = %s"
-                await cursor.execute(sql, (data['email']))
-                await conn.commit()
-                await server_stream.send_all(b'Server do the action')
+        # Иначе отправляются данные с объяснениями клиенту об ошибке.
+        await send_json_to_client(server_stream, result)
 
 
 # Аутентификация пользователя и генерация токена
 async def login(server_stream, data: dict):
-    async with connection as conn:
-        async with conn.cursor() as cursor:
-            sql = "SELECT id FROM users WHERE email = %s and password = %s"
-            await cursor.execute(sql, (data['email'], await get_hash_password(data['password'])))
-            user_id = await cursor.fetchone()
-            if user_id is None:
-                await server_stream.send_all(b'LOGIN ERROR')
-            else:
-                token = await generate_token(user_id)
-                await send_json_to_client(server_stream, token)
+    # Проверка данных на чистоту, если всё в порядке продолжаем аутентификацию
+    result = await clean_login(data)
+    if result['flag'] == 'true':
+        async with connection as conn:
+            async with conn.cursor() as cursor:
+                sql = "SELECT id FROM users WHERE email = %s and password = %s"
+                # Делаем запрос в БД
+                await cursor.execute(sql, (data['email'], await get_hash_password(data['password'])))
+                # Получаем результат сделаного запроса
+                user_id = await cursor.fetchone()
+                if user_id is None:
+                    await server_stream.send_all(b'LOGIN ERROR')
+                else:
+                    token = await generate_token(user_id)
+                    await send_json_to_client(server_stream, token)
+    else:
+        # Иначе отправляются данные с объяснениями клиенту об ошибке.
+        await send_json_to_client(server_stream, result)
 
 
 # Регистрация нового пользователя
 async def registration(server_stream, data: dict):
-    async with connection as conn:
-        async with conn.cursor() as cursor:
-            sql_check = "SELECT id FROM users WHERE email = %s or nickname = %s"
-            await cursor.execute(sql_check,
-                                 (data['email'], data['nickname']))
-            user_exist = await cursor.fetchone()
-            if user_exist:
-                result = {'result': 'user exist'}
-                await send_json_to_client(server_stream, result)
-            else:
-                sql_insert = "INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)"
-                await cursor.execute(sql_insert,
-                                     (data['email'], await get_hash_password(data['password']), data['nickname']))
-                # connection is not autocommit by default. So you must commit to save
-                # your changes.
-                await conn.commit()
-                result = {'result': 'success'}
-                await send_json_to_client(server_stream, result)
+    # Проверка данных на чистоту, если всё в порядке продолжаем регистрацию
+    result = await clean_registration(data)
+    if result['flag'] == 'true':
+        async with connection as conn:
+            async with conn.cursor() as cursor:
+                sql_check = "SELECT id FROM users WHERE email = %s or nickname = %s"
+                # Делаем запрос в БД
+                await cursor.execute(sql_check,
+                                     (data['email'], data['nickname']))
+                # Получаем результат сделаного запроса
+                user_exist = await cursor.fetchone()
+                if user_exist:
+                    result = {'result': 'user exist'}
+                    await send_json_to_client(server_stream, result)
+                else:
+                    sql_insert = "INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)"
+                    # Делаем запрос в БД
+                    await cursor.execute(sql_insert,
+                                         (data['email'], await get_hash_password(data['password']), data['nickname']))
+                    # Соединение не автокомитится по умолчанию.
+                    # Поэтому нужно сделать комит для сохранения данных в БД
+                    await conn.commit()
+                    result = {'result': 'user is registered'}
+                    await send_json_to_client(server_stream, result)
+    else:
+        # Иначе отправляются данные с объяснениями клиенту об ошибке.
+        await send_json_to_client(server_stream, result)
 
 
 # Интерфейс для обработки данных от клиента
 async def parse_client_data(server_stream, data: bytes):
     client_data = json.loads(data)
-    clean_data = await filter_client_data(client_data)
-    if clean_data['flag'] == 'true':
-        if client_data['client'] == 'act':
-            await action(server_stream, client_data)
-        elif client_data['client'] == 'log':
-            await login(server_stream, client_data)
-        elif client_data['client'] == 'reg':
-            await registration(server_stream, client_data)
+    if client_data['client'] == 'act':
+        await action(server_stream, client_data)
+    elif client_data['client'] == 'log':
+        await login(server_stream, client_data)
+    elif client_data['client'] == 'reg':
+        await registration(server_stream, client_data)
     else:
-        await send_json_to_client(server_stream, clean_data)
+        result = {'result': 'wrong data in main field'}
+        await send_json_to_client(server_stream, result)
 
 
 # Получение данных от клиента
