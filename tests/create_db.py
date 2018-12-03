d8389ee026a408f52bd34c95f38786cf76144913
aever32 <aeveraeveraever@gmail.com>
2018-12-03 13:41:24 +0200

    Корректировал поля в БД таблицы users

--- tests/create_db.py
+++ tests/create_db.py
@@ -13,7 +13,7 @@ try:
                 id int(11) unsigned PRIMARY KEY AUTO_INCREMENT,
                 email varchar(255) NOT NULL UNIQUE KEY,
                 password char(64) NOT NULL,
-                nickname char(20) NOT NULL UNIQUE KEY,
+                nickname varchar(30) NOT NULL UNIQUE KEY,
                 phone_num varchar(15) UNIQUE KEY,
                 created TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP,
                 updated TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
