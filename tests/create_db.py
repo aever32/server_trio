f9bfbfb486405bab2e48a27cc99d87690ec1e5f0
aever32 <aeveraeveraever@gmail.com>
2018-12-04 15:34:42 +0200

    Изменил поля в БД таблицы users

--- tests/create_db.py
+++ tests/create_db.py
@@ -2,23 +2,28 @@ import mysql.connector
 
 connection = mysql.connector.connect(user='root',
                                      password='root',
-                                     host='127.0.0.1')
+                                     host='127.0.0.1',
+                                     charset='utf8mb4')
 cursor = connection.cursor()
 
 try:
-    create_db = """
+    sql_1 = """
                 CREATE DATABASE IF NOT EXISTS game;
+                """
+    sql_2 = """
                 USE game;
                 CREATE TABLE IF NOT EXISTS users (
-                id int(11) unsigned PRIMARY KEY AUTO_INCREMENT,
-                email varchar(255) NOT NULL UNIQUE KEY,
+                id int unsigned PRIMARY KEY AUTO_INCREMENT,
+                email varchar(128) NOT NULL UNIQUE KEY,
                 password char(64) NOT NULL,
                 nickname varchar(30) NOT NULL UNIQUE KEY,
-                phone_num varchar(15) UNIQUE KEY,
+                phone_num varchar(15) DEFAULT 0 UNIQUE KEY,
                 created TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP,
                 updated TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-                );"""
-    cursor.execute(create_db)
+                );
+                """
+    cursor.execute(sql_1)
+    cursor.execute(sql_2)
 
 except mysql.connector.Error as err:
     print("Failed: {}".format(err))
