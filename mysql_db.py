9544ba36d986e44a0203009077e9fed227cc7a37
aever32 <aeveraeveraever@gmail.com>
2018-12-05 16:47:24 +0200

    Добавил механизм транзакций

--- mysql_db.py
+++ mysql_db.py
@@ -14,22 +14,39 @@ connection = trio_mysql.connect(**DB_CONFIG)
 
 
 async def execute(cursor, command, arguments):
+    await cursor.execute(command, arguments)
     try:
-        await cursor.execute(command, arguments)
         result = await cursor.fetchall()
-    except trio_mysql.err.MySQLError as error:
+    except trio_mysql.err.ProgrammingError as error:
         print('error in MYSQL execute: {}'.format(error))
         result = None
     return result
 
 
 async def sql(command, arguments=None):
-    async with connection as conn:
+    async with connection() as conn:
         async with conn.cursor() as cursor:
             result = await execute(cursor, command, arguments)
             # FIXME временная заплатка для записи в БД
             # Нужно добавить автокомит или разобраться с ТРАНЗАКЦИЯМИ!
             # Соединение не автокомитится по умолчанию.
-            # Поэтому нужно сделать комит для сохранения данных в БД
+            # Поэтому нужно использовать комит для сохранения данных в БД
             await conn.commit()
+            return result
+
+
+async def transaction(callback, arguments):
+    async with connection as conn:
+        async with conn.cursor() as cursor:
+
+            await cursor.execute('BEGIN')
+
+            try:
+                result = await callback(arguments=arguments,
+                                        execute=lambda command, arguments=None: execute(cursor, command, arguments))
+            except BaseException:
+                await cursor.execute('ROLLBACK')
+                raise
+            else:
+                await cursor.execute('COMMIT')
             return result
