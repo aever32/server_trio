2ead4e9413b2ba469ab2c0bd67d632f0e02f209e
aever32 <aeveraeveraever@gmail.com>
2018-12-06 19:41:01 +0200

    Переделал код с использованием асинхронной библиотеки trio_mysq

--- tests/create_db.py
+++ tests/create_db.py
@@ -1,33 +1,42 @@
-import mysql.connector
+import trio
+import trio_mysql.cursors
 
-connection = mysql.connector.connect(user='root',
-                                     password='root',
-                                     host='127.0.0.1',
-                                     charset='utf8mb4')
-cursor = connection.cursor()
+DB_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "root",
+    "password": "root",
+    "charset": "utf8mb4",
+    "autocommit": "True",
+}
 
-try:
-    sql_1 = """
-                CREATE DATABASE IF NOT EXISTS game;
-                """
-    sql_2 = """
-                USE game;
-                CREATE TABLE IF NOT EXISTS users (
-                id int unsigned PRIMARY KEY AUTO_INCREMENT,
-                email varchar(128) NOT NULL UNIQUE KEY,
-                password char(64) NOT NULL,
-                nickname varchar(30) NOT NULL UNIQUE KEY,
-                phone_num varchar(15) NULL UNIQUE KEY,
-                created TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP,
-                updated TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-                );
-                """
-    cursor.execute(sql_1)
-    cursor.execute(sql_2)
+connection = trio_mysql.connect(**DB_CONFIG)
 
-except mysql.connector.Error as err:
-    print("Failed: {}".format(err))
-    exit(1)
 
-cursor.close()
-connection.close()
+async def main():
+    async with connection as conn:
+        async with conn.cursor() as cursor:
+            try:
+                sql_1 = """
+                            CREATE DATABASE IF NOT EXISTS game;
+                            """
+                sql_2 = """
+                            USE game;
+                            CREATE TABLE IF NOT EXISTS users (
+                            id int unsigned PRIMARY KEY AUTO_INCREMENT,
+                            email varchar(128) NOT NULL UNIQUE KEY,
+                            password char(64) NOT NULL,
+                            nickname varchar(30) NOT NULL UNIQUE KEY,
+                            phone_num varchar(15) NULL UNIQUE KEY,
+                            created TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP,
+                            updated TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                            );
+                            """
+                await cursor.execute(sql_1)
+                await cursor.execute(sql_2)
+
+            except trio_mysql.err.MySQLError as err:
+                print("Failed: {}".format(err))
+                exit(1)
+
+trio.run(main)
