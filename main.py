bc5f28628c86e79f7c5e85813dcf03cadea06960
aever32 <aeveraeveraever@gmail.com>
2018-12-07 22:18:03 +0200

    Блок проверки данных пользователя перемещен в другой файл. Добавлена проверка на некорректные данные при парсинге json. Добавлена показательная функция использования транзакций, при следующем коммите ее можно убрать.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import json
 # import logging
 import secrets
 import trio
-import mysql_db
-from check_registration import clean_registration
+from mysql_db import (sql, transaction)
+from check_client_data import (clean_registration, clean_login, clean_action)
 
 HOST = '0.0.0.0'
 PORT = 12345
@@ -13,23 +13,8 @@ BUF_SIZE = 2048
 # Глобальный словарь {id пользователя: токен}
 TOKENS = dict()
 
-
 # FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
 # logging.basicConfig(format=FORMAT, filename='logs.log')
-
-
-# Проверка блока аутентификации
-async def clean_login(data: dict) -> dict:
-    return {'result': 'true'}
-
-
-# Проверка блока действий
-async def clean_action(data: dict) -> dict:
-    # Сверка данных токена от пользователя с глобальным словарем токенов сервера
-    if data['token'] == TOKENS.get(data['id']):
-        return {'result': 'true'}
-    else:
-        return {'result': 'false'}
 
 
 # Генерация хэш пароля
@@ -69,7 +54,7 @@ async def action(server_stream, data: dict):
         else:
             query = "UPDATE users SET updated=now() WHERE email = %s"
             # Делаем запрос в БД
-            await mysql_db.sql(query, (data['email']))
+            await sql(query, (data['email']))
             result = {'result': 'server do the action'}
             await send_json_to_client(server_stream, result)
     else:
@@ -77,14 +62,37 @@ async def action(server_stream, data: dict):
         await send_json_to_client(server_stream, result)
 
 
+# Пример функции с нужными запросами внутри транзакции
+async def do_in_transaction(execute, arguments):
+    my_arg0 = arguments[0]
+    my_arg1 = arguments[1]
+    my_arg2 = arguments[2]
+    try:
+        result = await execute("INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)",
+                               (my_arg0, my_arg1, my_arg2))
+        print('Result 1 inside do_in_transaction() ->', result)
+        result = await execute("SELECT nickname FROM users")
+        print('Result 2 inside do_in_transaction() ->', result)
+        result = await execute("INSERT INTO users (email, password, nickname) VALUES ('a@q.ru', 'pas12345', 'name0')")
+        print('Result 3 inside do_in_transaction() ->', result)
+    except BaseException as err:
+        print('INSIDE do_in_trans -> ', err)
+
+
 # Аутентификация пользователя и генерация токена
 async def login(server_stream, data: dict):
     # Проверка данных на чистоту, если всё в порядке продолжаем аутентификацию
     result = await clean_login(data)
     if result['result'] == 'true':
+        arguments = ('www@mail.com', 'data123', 'test345')
+        # FIXME Пример использования функции транзакции, удалю при следующем коммите.
+        # Для запуска этого примера, просто введи на клиенте команду: log с любым email и password
+        result = await transaction(do_in_transaction, arguments)
+        print('РЕЗУЛЬТАТ ПОСЛЕ transaction()! ->', result)
         query = "SELECT id FROM users WHERE email = %s and password = %s"
         # Делаем запрос в БД и получаем результат
-        user_id = await mysql_db.sql(query, (data['email'], await get_hash_password(data['password'])))
+        user_id = await sql(query,
+                            (data['email'], await get_hash_password(data['password'])))
         if user_id:
             token = await generate_token(user_id)
             await send_json_to_client(server_stream, token)
@@ -103,15 +111,15 @@ async def registration(server_stream, data: dict):
     if result['result'] == 'true':
         query = "SELECT id FROM users WHERE email = %s or nickname = %s"
         # Делаем запрос в БД и получаем результат
-        user_exist = await mysql_db.sql(query,
-                                        (data['email'], data['nickname']))
+        user_exist = await sql(query,
+                               (data['email'], data['nickname']))
         if user_exist:
             result = {'result': 'user already exist'}
             await send_json_to_client(server_stream, result)
         else:
             query = "INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)"
-            result = await mysql_db.sql(query,
-                                        (data['email'], await get_hash_password(data['password']), data['nickname']))
+            result = await sql(query,
+                               (data['email'], await get_hash_password(data['password']), data['nickname']))
             if not result:
                 result = {'result': 'registration is successful'}
                 await send_json_to_client(server_stream, result)
@@ -122,16 +130,20 @@ async def registration(server_stream, data: dict):
 
 # Интерфейс для обработки данных от клиента
 async def parse_client_data(server_stream, data: bytes):
-    client_data = json.loads(data)
-    if client_data['client'] == 'act':
-        await action(server_stream, client_data)
-    elif client_data['client'] == 'log':
-        await login(server_stream, client_data)
-    elif client_data['client'] == 'reg':
-        await registration(server_stream, client_data)
-    else:
-        result = {'result': 'wrong data in main field'}
-        await send_json_to_client(server_stream, result)
+    try:
+        client_data = json.loads(data)
+        if client_data['client'] == 'act':
+            await action(server_stream, client_data)
+        elif client_data['client'] == 'log':
+            await login(server_stream, client_data)
+        elif client_data['client'] == 'reg':
+            await registration(server_stream, client_data)
+        else:
+            result = {'result': 'wrong data in main field'}
+            await send_json_to_client(server_stream, result)
+    except ValueError as e:
+        print(e)
+        await send_json_to_client(server_stream, {'result': 'error input data'})
 
 
 # Получение данных от клиента
